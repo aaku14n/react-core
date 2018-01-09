bdee823479a7a81e3d90badaba27c239de9b904b
y-aakarsh <aakarsh@xelpmoc.in>
2018-01-09 16:25:01 +0530

    Adding final config chages in web pack

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,22 @@
-const path = require("path");
-var webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: "./public/index.html",
-  filename: "index.html",
-  inject: "body"
-});
-
+var path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve("build"),
-    filename: "index.js"
+    path: path.resolve(__dirname, "build"),
+    filename: "index.js",
+    libraryTarget: "commonjs2" // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-        query: {
-          presets: ["es2015", "react", "stage-2"]
+        include: path.resolve(__dirname, "src"),
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["env"]
+          }
         }
       },
       {
@@ -28,29 +24,13 @@ module.exports = {
         loaders: [
           "style-loader",
           "css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]&sourceMap&-minimize"
-        ]
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf)$/,
-        loader: "url-loader?limit=100000",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(jpg|png|svg)$/,
-        loader: "file-loader",
-        options: {
-          name: "[path][name].[hash].[ext]"
-        },
-        exclude: /node_modules/,
+        ],
+        include: __dirname + "/src",
+        exclude: /node_modules/
       }
     ]
   },
-  plugins: [HtmlWebpackPluginConfig],
-  devServer: {
-    colors: true,
-    historyApiFallback: true,
-    inline: true,
-    hot: true
+  externals: {
+    react: "commonjs react" // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
   }
 };
