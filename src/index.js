a47a289d62ce7497716d1a7e04696b4ac79db951
aaku14n <akarshgdps@gmail.com>
2018-04-23 22:42:32 +0530

    Adding changes

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import Input from "./Input";
 import Button from "./Button";
 import ChatBox from "./ChatBox";
 import Icon from "./Icon";
+import Grid from "./Grid";
 
-module.exports = { Input, Button, Icon, ChatBox };
+module.exports = { Input, Button, Icon, ChatBox, Grid };
